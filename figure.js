44e2e5d4765efe69958d5a93dc2e3702b03c88e3
ERRrOR404 <error99998@gmail.com>
2021-08-13 11:01:25 +0900

    feeat: draw former rects

--- figure.js
+++ figure.js
@@ -2,52 +2,112 @@ export class Figure {
     constructor() {
         this.isClick = false;
         this.sqr = null;
-        this.start = {x: undefined, y: undefined};
-        this.end = {x: undefined, y: undefined};
+
+        this.start = {
+            x: undefined,
+            y: undefined,
+        };
+
+        this.end = {
+            x: undefined,
+            y: undefined,
+        };
+
+        this.rects = [];
     }
 
     update(event) {
         if (this.isClick) {
-            this.start = {x: event.clientX, y: event.clientY};
+            this.start = {
+                x: event.clientX,
+                y: event.clientY,
+            };
 
             this.isClick = null;
         } 
         
         if (this.isClick !== false) {
-            this.end = {x: event.clientX, y: event.clientY};
+            this.end = {
+                x: event.clientX,
+                y: event.clientY,
+            };
         }
     }
 
     startClick() {
         this.isClick = true;
+
+        this.start = {
+            x: undefined,
+            y: undefined,
+        };
+        this.end = {
+            x: undefined,
+            y: undefined,
+        };
     }
 
     endClick() {
+        this.sqr = this.getSquare(this.start, this.end);
+
         this.isClick = false;
 
-        this.start = {x: undefined, y: undefined};
-        this.end = {x: undefined, y: undefined};
+        this.rects.push({
+            vt1: {
+                x: this.start.x,
+                y: this.start.y,
+            },
+            vt2: {
+                x: this.sqr[0].x,
+                y: this.sqr[0].y,
+            },
+            vt3: {
+                x: this.sqr[1].x,
+                y: this.sqr[1].y,
+            },
+            vt4: {
+                x: this.end.x,
+                y: this.end.y,
+            },
+        });
+
+        console.log(this.rects);
     }
 
     draw(ctx) {
-        if (this.isClick === false || this.end.x === null || this.end.y === null) {
-            return;
-        }
+        if (this.isClick !== false && this.end.x !== null || this.end.y !== null) {
+            this.sqr = this.getSquare(this.start, this.end);
 
-        this.sqr = this.getSquare(this.start, this.end);
-
-        ctx.beginPath();
-        ctx.fillStyle = '#ffffff';
-
-        ctx.moveTo(this.start.x, this.start.y);
-        ctx.lineTo(this.sqr[0].x, this.sqr[0].y);
-        ctx.lineTo(this.sqr[1].x, this.sqr[1].y);
+            ctx.beginPath();
+            ctx.fillStyle = '#ffffff';
     
-        ctx.moveTo(this.end.x, this.end.y);
-        ctx.lineTo(this.sqr[0].x, this.sqr[0].y);
-        ctx.lineTo(this.sqr[1].x, this.sqr[1].y);
-        ctx.fill();
-        ctx.closePath();
+            ctx.moveTo(this.start.x, this.start.y);
+            ctx.lineTo(this.sqr[0].x, this.sqr[0].y);
+            ctx.lineTo(this.sqr[1].x, this.sqr[1].y);
+        
+            ctx.moveTo(this.end.x, this.end.y);
+            ctx.lineTo(this.sqr[0].x, this.sqr[0].y);
+            ctx.lineTo(this.sqr[1].x, this.sqr[1].y);
+            ctx.fill();
+            ctx.closePath();
+        }
+        
+        for (let i = 0; i < this.rects.length; i++) {
+            this.sqr = this.getSquare(this.rects[i].vt1, this.rects[i].vt4);
+            
+            ctx.beginPath();
+            ctx.fillStyle = '#ff0000';
+    
+            ctx.moveTo(this.rects[i].vt1.x, this.rects[i].vt1.y);
+            ctx.lineTo(this.rects[i].vt2.x, this.rects[i].vt2.y);
+            ctx.lineTo(this.rects[i].vt3.x, this.rects[i].vt3.y);
+        
+            ctx.moveTo(this.rects[i].vt4.x, this.rects[i].vt4.y);
+            ctx.lineTo(this.rects[i].vt2.x, this.rects[i].vt2.y);
+            ctx.lineTo(this.rects[i].vt3.x, this.rects[i].vt3.y);
+            ctx.fill(); 
+            ctx.closePath();
+        }
     }
 
     getSquare(a, b) {
